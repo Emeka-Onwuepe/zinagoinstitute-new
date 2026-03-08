de4d8e601cd681c59cdc1841a89dd2941dc95ab5
EMEKA ONWUEPE <pascalemy2010@gmail.com>
2026-03-08 12:56:42 +0100

    settings

--- zinagoinstitute/settings.py
+++ zinagoinstitute/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-!ram&xirg0@xih0ckp$o(k*oe#ie_!xfqr-qp%h^ux7*#qd$=)
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['app.lotzcrocoz.com','127.0.0.1']
 
 
 # Application definition
