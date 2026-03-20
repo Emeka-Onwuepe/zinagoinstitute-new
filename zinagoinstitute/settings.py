930870501326fb155c7c55e309b7aa368bf567c5
EMEKA ONWUEPE <pascalemy2010@gmail.com>
2026-03-20 23:55:38 +0100

    domain change

--- zinagoinstitute/settings.py
+++ zinagoinstitute/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = os.environ.get('secret_key')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['app.lotzcrocoz.com','127.0.0.1']
+ALLOWED_HOSTS = ['zinagoinstitute.org','127.0.0.1']
 
 
 # Application definition
